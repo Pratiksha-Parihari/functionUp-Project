74d2a282ada1d3b9e7ae9851af97a89188d68b20
Pratiksha Parihari <parihari.pratiksha17@gmail.com>
2022-11-28 15:36:09 +0530

    Update userModel.js

---
+++ src/models/userModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    
+        title: {
+            type:String, 
+            required:true, 
+            enum: [ "Mr", "Mrs", "Miss"]
+        },
+        name: {
+            type:String, 
+            required:true
+        },
+        phone: {
+            type: Number, 
+            required:true, 
+            unique: true
+        },
+        email: {
+            type:String, 
+            required:true, 
+            trim:true, 
+            unique:true
+        }, 
+        password: {
+            type:String, 
+            required:true, 
+            minLength: 8, 
+            maxLength : 15
+        },
+        address: {
+          street: String,
+          city: String,
+          pincode: String
+        }
+        
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', userSchema)
+
